16d39f2b9d3d956ca2227bc14a362d2b227d61a6
kcotten <kris.cotten@gmail.com>
2021-09-13 12:52:29 -0700

    Flesh out client/server

--- widget_client/main.go
+++ widget_client/main.go
@@ -1,1 +1,48 @@
 package widgetclient
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "grpclib/widget"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:9890"
+)
+
+// Code for the rest endpoint to send request to the storage service
+// Alternately all the code could be in a monolithic package
+// 1. Unmarshall to protobuf
+// 2. Send request
+// 3. Marshall response
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProcessClient(conn)
+
+	// Contact the grpc server
+	name := "item1"
+	quantity := 7
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Update(ctx, &pb.UpdateRequest{Name: name, Quantity: int64(quantity)})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Greeting: %s", r.GetMessage())
+}
--- widget_server/main.go
+++ widget_server/main.go
@@ -1,7 +1,13 @@
 package widgetserver
 
 import (
-	pb "github.com/kcotten/grpclib/widget"
+	"context"
+	"log"
+	"net"
+
+	pb "grpclib/widget"
+
+	"google.golang.org/grpc"
 )
 
 const (
@@ -10,4 +16,28 @@ const (
 
 type server struct {
 	pb.UnimplementedProcessServer
+}
+
+func (s *server) Update(ctx context.Context, in *pb.UpdateRequest) (*pb.UpdateResponse, error) {
+	// Do something with the request, send to database, cache, etc...
+
+	response := "ok"
+	return &pb.UpdateResponse{Message: response}, nil
+}
+
+// Code to be placed in the storage service to listen for requests to update
+func main() {
+	// Listen for grpc client requests
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterProcessServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
